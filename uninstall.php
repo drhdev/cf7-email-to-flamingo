dcb526352972d906c4cd7b8efa1d8aa2bd54a9b2
drhdev <96695448+drhdev@users.noreply.github.com>
2024-10-28 21:56:43 +0100

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,8 @@
+<?php
+// If uninstall not called from WordPress, exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+// Delete plugin options
+delete_option( 'cf7_email_to_flamingo_settings' );
